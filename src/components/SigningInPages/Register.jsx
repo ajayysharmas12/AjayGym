2d706e7a0f15e66cbc8474b5a8c9c19c92cc0397
Souptik18 <souptiksarkar2017@gmail.com>
2024-05-06 00:26:52 +0530

    update

--- src/components/SigningInPages/Register.jsx
+++ src/components/SigningInPages/Register.jsx
@@ -108,12 +108,12 @@ const Register = () => {
           width: "screen",
         }}
       >
-        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+        <div className=" flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div
             style={{ boxShadow: "rgba(255, 255, 255, 1.35) 0px 0px 14px" }}
             className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md rounded-xl p-4"
           >
-            <h2 className="text-center text-2xl font-bold leading-tight text-neutral-200">
+            <h2 className="text-center text-2xl font-bold leading-tight text-neutral-900">
               Sign up to create an account
             </h2>
             <p className="mt-2 text-center text-base text-white">
